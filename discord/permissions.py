c390fd82c6f80da8a41af8730f8bb5f273c18031
noaione <noaione0809@gmail.com>
2021-09-17 18:53:15 +0800

    Add `start_embedded_activities` to permissions

--- discord/permissions.py
+++ discord/permissions.py
@@ -554,6 +554,14 @@ class Permissions(BaseFlags):
         """
         return 1 << 38
 
+    @flag_value
+    def start_embedded_activities(self) -> int:
+        """:class:`bool`: Returns ``True`` if a user can launch activities in a voice channel
+
+        .. versionadded:: 2.0
+        """
+        return 1 << 39
+
 
 PO = TypeVar("PO", bound="PermissionOverwrite")
 
@@ -669,6 +677,7 @@ class PermissionOverwrite:
         send_messages_in_threads: Optional[bool]
         external_stickers: Optional[bool]
         use_external_stickers: Optional[bool]
+        start_embedded_activities: Optional[bool]
 
     def __init__(self, **kwargs: Optional[bool]):
         self._values: Dict[str, Optional[bool]] = {}
