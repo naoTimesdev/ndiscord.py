4e30b546e835cd826bac893d23126e2fd0049d3c
noaione <noaione0809@gmail.com>
2021-11-16 14:07:18 +0800

    Remove SKU IDs

--- discord/audit_logs.py
+++ discord/audit_logs.py
@@ -73,10 +73,6 @@ def _transform_snowflake(entry: AuditLogEntry, data: Snowflake) -> int:
     return int(data)
 
 
-def _transform_snowflakes(data: List[Snowflake]) -> List[int]:
-    return [int(x) for x in data]
-
-
 def _transform_channel(entry: AuditLogEntry, data: Optional[Snowflake]) -> Optional[Union[abc.GuildChannel, Object]]:
     if data is None:
         return None
@@ -219,8 +215,7 @@ class AuditLogChanges:
         'format_type':                   (None, _enum_transformer(enums.StickerFormatType)),
         'type':                          (None, _transform_type),
         'entity_type':                   (None, _enum_transformer(enums.GuildScheduledEventType)),
-        'status':                        (None, _enum_transformer(enums.GuildScheduledEventStatus)),
-        'sku_ids':                       (None, _transform_snowflakes)
+        'status':                        (None, _enum_transformer(enums.GuildScheduledEventStatus))
     }
     # fmt: on
 
